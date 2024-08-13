87409a09469c7fec0226f59be75563f8060b175e
ml-hossain <hossain890m@gmail.com>
2024-08-13 15:48:35 +0600

    written some code

---
+++ Day 25/app.js
@@ -0,0 +1,52 @@
+// for(let i = 1; i < 11; i++){
+//     console.log(i);
+// }
+
+// for(let i = 25; i < 51; i++){
+//     console.log(i);
+// }
+
+// for(let i = 35; i <= 40; i++){
+//     console.log(i);
+// }
+// for(let i = 20; i > 4; i--){
+//     console.log(i);
+// }
+// for(let i = 5; i > 0; i--){
+//     console.log(i);
+// }
+
+
+// start;
+// while(end){
+//     change;
+// }
+
+
+// start; 
+// while(end){
+//     change;
+// }
+// start;
+// while(end){
+//     change;
+// }
+
+// start;
+// while(end){
+//     change;
+
+
+
+// }
+
+
+
+
+
+let age = 1;
+
+while(age < 11){
+    age++;
+    
+}
